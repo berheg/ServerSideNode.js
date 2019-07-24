8fc77bd3b548f6c112a62e6ded218eda448c161c
Girmay Hailu Berhe <48474199+berheg@users.noreply.github.com>
2019-07-24 20:57:10 +0200

    server post as request added

--- main.js
+++ main.js
@@ -2,3 +2,6 @@ const express = require('express');
 const app = express();
 app.listen(3000, () => console.log('Listening at 3000'));
 app.use(express.static('public'));
+app.post('/api', (request, response) => {
+    console.log(request);
+})
