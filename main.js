387b59196eae6e7ff22a4001718aaec4fa8444ee
Girmay Hailu Berhe <48474199+berheg@users.noreply.github.com>
2019-07-24 21:21:40 +0200

    response data added

--- main.js
+++ main.js
@@ -2,6 +2,14 @@ const express = require('express');
 const app = express();
 app.listen(3000, () => console.log('Listening at 3000'));
 app.use(express.static('public'));
+app.use(express.json({ limit: 'lmb'})); 
 app.post('/api', (request, response) => {
-    console.log(request);
+    console.log('I got a request!');
+    console.log(request.body);
+    const data =request.body;
+    response.json({
+        status: 'success',
+        latitude: data.lat,
+        longitude: data.lon
+    });
 })
